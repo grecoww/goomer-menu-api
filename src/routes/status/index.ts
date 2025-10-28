0747faf08fb008586a4bfc2b89eeb37fdb2f7db9
grecoww <132113259+grecoww@users.noreply.github.com>
2025-10-28 00:20:26 -0300

    feat: add database info in status

--- src/routes/status/index.ts
+++ src/routes/status/index.ts
@@ -1,7 +1,12 @@
 import { type FastifyInstance } from 'fastify'
+import database from '../../../infra/database'
 
 export default async function statusRoute(app: FastifyInstance) {
     app.get('/status', async (_, reply) => {
-        reply.code(200).send({ status: 'alive' })
+        const response = await database.query('SHOW server_version;')
+        const databaseInfo = response.rows[0].server_version
+        reply
+            .code(200)
+            .send({ status: 'alive', database_version: databaseInfo })
     })
 }
