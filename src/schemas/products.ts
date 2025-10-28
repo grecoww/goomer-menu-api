ccd8ee0ca0c08283c4a25c59c7849969c4556c53
grecoww <132113259+grecoww@users.noreply.github.com>
2025-10-28 15:10:07 -0300

    refac: zod schemas less redundant in `products`

--- src/schemas/products.ts
+++ src/schemas/products.ts
@@ -8,13 +8,41 @@ const ProductCategory = [
     'bebidas',
 ] as const
 
-export const ProductInputSchema = {
-    body: z.object({
-        name: z.string().max(255),
-        price_cents: z.int().min(0),
-        category: z.enum(ProductCategory),
-        visibility: z.boolean(),
-    }),
+const ProductInputSchema = z.object({
+    name: z.string().max(255),
+    price_cents: z.int().min(0),
+    category: z.enum(ProductCategory),
+    visibility: z.boolean(),
+})
+export type ProductInput = z.infer<typeof ProductInputSchema>
+
+const CompleteProductSchema = ProductInputSchema.extend({
+    id: z.int().min(1),
+    created_at: z.date(),
+    updated_at: z.date(),
+})
+
+export const ProductCreationSchema = {
+    body: ProductInputSchema,
+    response: {
+        201: {
+            content: {
+                'application/json': {
+                    schema: CompleteProductSchema,
+                },
+            },
+        },
+    },
 } satisfies FastifyZodOpenApiSchema
 
-export type ProductInput = z.infer<typeof ProductInputSchema.body>
+export const ProductListSchema = {
+    response: {
+        200: {
+            content: {
+                'application/json': {
+                    schema: z.array(CompleteProductSchema),
+                },
+            },
+        },
+    },
+} satisfies FastifyZodOpenApiSchema
